2f2cced157331bfef26b3e060b72aedd9efbb3d8
SamTamer <122141054+SamTamer@users.noreply.github.com>
2026-07-23 00:08:04 -0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -27,7 +27,7 @@ if [ ! -d "venv" ]; then
 fi
 
 # 3. Activate virtual environment
-source venv/bin/activate
+#source "$(dirname "$0")/venv/bin/activate"
 
 # 4. Install dependencies
 echo "Installing dependencies..."
